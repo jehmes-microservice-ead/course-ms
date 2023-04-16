ca6cd4ff02f15d610e9a3675edced1c78d6a3c12
jehmes <jeihmesthales@hotmail.com>
2023-04-15 23:36:27 -0300

    Created methods to new communications

--- src/main/java/com/ead/course/services/UserService.java
+++ src/main/java/com/ead/course/services/UserService.java
@@ -1,5 +1,13 @@
 package com.ead.course.services;
 
+import com.ead.course.models.UserModel;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+
+import java.util.Optional;
+
 public interface UserService {
 
+    Page<UserModel> findAll(Specification<UserModel> spec, Pageable pageable);
 }
